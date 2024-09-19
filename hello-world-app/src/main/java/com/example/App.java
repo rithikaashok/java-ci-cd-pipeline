ef1942ab0a77844ce8e53585ebbacb90dfb73cf0
rithikaashok <rithikaashokbabu06@gmail.com>
2024-09-19 12:54:08 +0530

    Add hello-world-app with Dockerfile

---
+++ hello-world-app/src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example.hello_world_app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ hello-world-app/src/test/java/com/example/hello_world_app/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_world_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
